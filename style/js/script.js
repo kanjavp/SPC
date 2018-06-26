2b3f39848723f824951e769de56a68d6e5e128bf
Kanjav <39911716+kanjavp@users.noreply.github.com>
2018-06-26 14:27:11 -0400

    Added Bradning
    Changed Colors of the website and UX/UI

---
+++ style/js/script.js
@@ -0,0 +1,96 @@
+/*window.onload = colorChange;*/
+
+console.log("JS is ON\n");
+
+$(document).ready(function () {
+
+
+
+    $(".initiator").hover(function(){
+        $(".receiver").toggleClass("hehe");
+        console.log("div was hovered");
+    });
+
+    $("#c1").hover(function(){
+        $("#f1").toggleClass("myFooterHover");
+        console.log("myFooter was hovered");        $("#cht1").toggleClass("clickHereTextHover");
+        console.log("clickHereText2 was hovered");
+        $("#l1").toggleClass("testimonialsLineHover");
+        console.log("testimonialsLineHover was hovered");     
+    });
+    $("#c2").hover(function(){
+        $("#f2").toggleClass("myFooterHover");
+        console.log("myFooter was hovered");        $("#cht2").toggleClass("clickHereTextHover");
+        console.log("clickHereText2 was hovered");
+        $("#l2").toggleClass("testimonialsLineHover");
+        console.log("testimonialsLineHover was hovered");     
+    });
+    $("#c3").hover(function(){
+        $("#f3").toggleClass("myFooterHover");
+        console.log("myFooter was hovered");        $("#cht3").toggleClass("clickHereTextHover");
+        console.log("clickHereText2 was hovered");
+        $("#l3").toggleClass("testimonialsLineHover");
+        console.log("testimonialsLineHover was hovered");     
+    });
+    $("#c4").hover(function(){
+        $("#f4").toggleClass("myFooterHover");
+        console.log("myFooter was hovered");        $("#cht4").toggleClass("clickHereTextHover");
+        console.log("clickHereText2 was hovered");
+        $("#l4").toggleClass("testimonialsLineHover");
+        console.log("testimonialsLineHover was hovered");     
+    });
+    $("#c5").hover(function(){
+        $("#f5").toggleClass("myFooterHover");
+        console.log("myFooter was hovered");        $("#cht5").toggleClass("clickHereTextHover");
+        console.log("clickHereText2 was hovered");
+        $("#l5").toggleClass("testimonialsLineHover");
+        console.log("testimonialsLineHover was hovered");     
+    });
+    $("#c6").hover(function(){
+        $("#f6").toggleClass("myFooterHover");
+        console.log("myFooter was hovered");        $("#cht6").toggleClass("clickHereTextHover");
+        console.log("clickHereText2 was hovered");
+        $("#l6").toggleClass("testimonialsLineHover");
+        console.log("testimonialsLineHover was hovered");     
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function colorChange(){
+    var transitionFontColor = document.getElementsByClassName("myFooter");console.log("Hi_2");
+    document.getElementsByClassName("myCard").onmouseover = function hi() {
+        mouseOver();
+    };
+    console.log("Hi_3");
+    // transitionFontColor[0].innerHTML = "Hello World";
+    // var myCardBox = document.getElementsByClassName('myCard').onmouseover;
+    function mouseOver() {
+        console.log("Hi_4");
+        alert("Hi_5");
+        document.getElementById("myCard").style.color = "blue";
+    }
+
+
+    if(myCardHTML == true){     
+        console.log("It works/nHehe"); 
+    }
+    else{
+        console.log("Does not work!");   
+    }
+}*/
+
